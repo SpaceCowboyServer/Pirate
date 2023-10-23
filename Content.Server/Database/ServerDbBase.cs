4e18b16712bf3880ee0716dc812fd693e0eee311
LankLTE <135308300+LankLTE@users.noreply.github.com>
2023-10-22 19:14:37 -0700

    height slider (#1)
    * Basic Implementation
    
    * Working implementation.

--- Content.Server/Database/ServerDbBase.cs
+++ Content.Server/Database/ServerDbBase.cs
@@ -210,6 +210,7 @@ namespace Content.Server.Database
                 profile.CharacterName,
                 profile.FlavorText,
                 profile.Species,
+                profile.Height,
                 profile.Age,
                 sex,
                 gender,
@@ -249,6 +250,7 @@ namespace Content.Server.Database
             profile.Age = humanoid.Age;
             profile.Sex = humanoid.Sex.ToString();
             profile.Gender = humanoid.Gender.ToString();
+            profile.Height = humanoid.Height;
             profile.HairName = appearance.HairStyleId;
             profile.HairColor = appearance.HairColor.ToHex();
             profile.FacialHairName = appearance.FacialHairStyleId;

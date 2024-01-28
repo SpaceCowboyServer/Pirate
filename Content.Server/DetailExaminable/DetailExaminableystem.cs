73f23933dfb6af7dc0ae4aac42a4631b5003f6bd
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-01-28 02:07:43 -0800

    update the commented detailExaminableSystem

--- Content.Server/DetailExaminable/DetailExaminableystem.cs
+++ Content.Server/DetailExaminable/DetailExaminableystem.cs
@@ -1,26 +1,29 @@
 ﻿// Parkstation-CharacterInformation-Start
 // using Content.Shared.Examine;
+// using Content.Shared.IdentityManagement;
 // using Content.Shared.Verbs;
 // using Robust.Shared.Utility;
-//
+
 // namespace Content.Server.DetailExaminable
 // {
 //     public sealed class DetailExaminableSystem : EntitySystem
 //     {
 //         [Dependency] private readonly ExamineSystemShared _examineSystem = default!;
-//
+
 //         public override void Initialize()
 //         {
 //             base.Initialize();
-//
+
 //             SubscribeLocalEvent<DetailExaminableComponent, GetVerbsEvent<ExamineVerb>>(OnGetExamineVerbs);
 //         }
-//
+
 //         private void OnGetExamineVerbs(EntityUid uid, DetailExaminableComponent component, GetVerbsEvent<ExamineVerb> args)
 //         {
-//             // TODO: Hide if identity isn't visible (when identity is merged)
+//             if (Identity.Name(args.Target, EntityManager) != MetaData(args.Target).EntityName)
+//                 return;
+
 //             var detailsRange = _examineSystem.IsInDetailsRange(args.User, uid);
-//
+
 //             var verb = new ExamineVerb()
 //             {
 //                 Act = () =>
@@ -32,10 +35,10 @@
 //                 Text = Loc.GetString("detail-examinable-verb-text"),
 //                 Category = VerbCategory.Examine,
 //                 Disabled = !detailsRange,
-//                 Message = Loc.GetString("detail-examinable-verb-disabled"),
+//                 Message = detailsRange ? null : Loc.GetString("detail-examinable-verb-disabled"),
 //                 Icon = new SpriteSpecifier.Texture(new ("/Textures/Interface/VerbIcons/examine.svg.192dpi.png"))
 //             };
-//
+
 //             args.Verbs.Add(verb);
 //         }
 //     }
